e62414091f0c0c0766775c0c08901ea29028fd61
somename <someemail>
2017-06-01 20:51:32 +0200

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -21,8 +21,17 @@ public class StringCalculator {
 				rezultat=rezultat+Integer.parseInt(niz[i]);
 			}
 			return rezultat;
+		} else if(numbersStr.contains("\n")){
+			
+			String[] niz=numbersStr.split("\n");
+			int rezultat=0;
+			for(int i=0;i<niz.length;i++){
+				
+				rezultat=rezultat+Integer.parseInt(niz[i]);
+			}
+			return rezultat;
 		}
-		return ;
+		return 0;
 	
 	}
 	
