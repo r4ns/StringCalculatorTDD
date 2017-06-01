aa778f11e11a26f92e767ec742d80fc559dfb03f
somename <someemail>
2017-06-01 19:55:52 +0200

    besouro automatic message

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_resultWhenSteringIsEmpty() {
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.add(" ");
+		assertEquals("Not valid result", 0, result);
 	}
 
 }
