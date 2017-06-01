c81c755401ebce9e3ba5ffba5812b80a21a98dba
somename <someemail>
2017-06-01 20:06:01 +0200

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -3,6 +3,8 @@ public class StringCalculator {
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
 		String[] separated = numbersStr.split(",");
+		String sep = separated.toString();
+		String[] novi = sep.split("\n");
 		if(numbersStr == " ")
 			return 0;
 		// not yet implemented
@@ -10,8 +12,8 @@ public class StringCalculator {
 		else{
 			int zbir=0;
 		
-			for(int i = 0; i<separated.length;i++){
-				zbir+=Integer.parseInt(separated[i]);
+			for(int i = 0; i<novi.length;i++){
+				zbir+=Integer.parseInt(novi[i]);
 			}
 			return zbir;
 		}
