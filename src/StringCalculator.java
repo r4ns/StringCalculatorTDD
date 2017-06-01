5ee83ca4011136edb494c09700089dfab6e56edd
ITC3 <ITC3@ws15.itc03.edu>
2017-06-01 20:55:26 +0200

    v1

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -11,4 +11,32 @@ public class StringCalculator {
 		
 	}
 	
+	
+	public String saberi(String str)
+	{
+		int rezultat=0;
+		String brojevi[]=str.split(",");
+		//String konacanNiz[];
+		
+		for(int i=0; i<brojevi.length; i++)
+		{
+			if (brojevi[i].contains("\n"))
+			{
+				String br[]=brojevi[i].split("\n");
+				
+				//i++;
+				//rezultat=Integer.parseInt(br[0])+Integer.parseInt(br[1])+Integer.parseInt(brojevi[i]);
+				rezultat=Integer.parseInt(brojevi[i]);
+			} else
+			{
+				rezultat+=Integer.parseInt(brojevi[i]);
+			}
+		}
+		
+		
+		
+		String rez= Integer.toString(rezultat);
+		return rez;
+		
+	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -16,4 +16,52 @@ public class StringCalculatorTest {
 		
 		assertNotNull(kalkulator);
 	}
+	
+	@Test
+	public void testSaberiSplitNaZarez()
+	{
+		StringCalculator kalkulator= new StringCalculator();
+		
+		
+		String rezultat=kalkulator.saberi("2,3");
+		
+		assertEquals("5", rezultat);
+	
+	}
+	
+	@Test
+	public void testSaberiSakosaCrtaN()
+	{
+		StringCalculator kalkulator= new StringCalculator();
+		
+		
+		String rezultat=kalkulator.saberi("1\n2,3");
+		
+		assertEquals("6", rezultat);
+	
+	}
+	
+	@Test 
+	public void testSaberiTribroja()
+	{
+		StringCalculator kalkulator= new StringCalculator();
+		
+		
+		String rezultat=kalkulator.saberi("2,3,3");
+		
+		assertEquals("8", rezultat);
+	}
+	
+	@Test
+	public void testSaberiSakosaCrtaNSaTriBroja()
+	{
+		StringCalculator kalkulator= new StringCalculator();
+		
+		
+		String rezultat=kalkulator.saberi("1\n2,3,4");
+		
+		assertEquals("10", rezultat);
+	
+	}
+	
 }
