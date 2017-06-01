1b96f8324424176c4f58e8308584b58dc2cb9a65
somename <someemail>
2017-06-01 20:37:04 +0200

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -12,7 +12,17 @@ public class StringCalculator {
 		}
 		else if(numbersStr.contains("-")){
 			throw new StringCalculatorException("Nije moguce vrsiti operacije sa negativnim brojevima");
+		} else {
+			
+			String[] niz=numbersStr.split(",");
+			int[] nizBrojeva=null;;
+			for(int i=0;i<niz.length;i++){
+				
+				nizBrojeva[i]=Integer.parseInt(niz[i]);
+			}
+			
 		}
+	
 		return 0;
 	}
 	
