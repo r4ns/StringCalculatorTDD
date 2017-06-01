d03a6b1fab102bdf5404d57b38cc727f7ab79ea2
somename <someemail>
2017-06-01 19:55:33 +0200

    besouro automatic message

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -7,7 +7,7 @@ public class StringCalculatorTest {
 	@Test
 	public void test_create_calculator() {
 		StringCalculator kalkulator=new StringCalculator();
-		assertNull(kalkulator);
+		assertNotNull(kalkulator);
 	}
 
 }
