eab3ea86cccdc39a41039244a4c6025adccab779
somename <someemail>
2017-06-01 20:26:16 +0200

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -3,17 +3,16 @@ import java.util.Arrays;
 public class StringCalculator {
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
-		String[] separated = numbersStr.split(",");
+		String[] separated = numbersStr.split(",\n");
 		String sep= "";
-		String[] novi={};
-		for(int i = 0; i<separated.length; i++)
-		{   sep +=separated[i];
-			if(separated[i]=="\n")
-				novi = sep.split("\n");
+		
+		/*for(int i = 0; i<separated.length; i++)
+		{
 			
+			sep +=separated[i];
 		}
-		
-		
+		*/
+		//String[] novi = sep.split("\n");
 		
 		if(numbersStr == " ")
 			return 0;
@@ -22,9 +21,9 @@ public class StringCalculator {
 		else{
 				int zbir=0;
 				
-				for(int i = 0; i<novi.length;i++){
+				for(int i = 0; i<separated.length;i++){
 				
-					zbir+=Integer.parseInt(novi[i]);
+					zbir+=Integer.parseInt(separated[i]);
 				
 			}
 			return zbir;
