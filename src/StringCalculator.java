66e89f1b31381f107e3a5caa13321e9a46ec4ca1
somename <someemail>
2017-06-01 20:08:04 +0200

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,18 +1,19 @@
+import java.util.Arrays;
 
 public class StringCalculator {
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
 		String[] separated = numbersStr.split(",");
-		String sep = separated.toString();
+		String sep = Arrays.toString(separated);
 		String[] novi = sep.split("\n");
 		if(numbersStr == " ")
 			return 0;
 		// not yet implemented
 		
 		else{
-			int zbir=0;
+				int zbir=0;
 		
-			for(int i = 0; i<novi.length;i++){
+				for(int i = 0; i<novi.length;i++){
 				zbir+=Integer.parseInt(novi[i]);
 			}
 			return zbir;
