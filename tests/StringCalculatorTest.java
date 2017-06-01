c17cffe308a59c71decbc0fa2d42c2ff97c83e79
somename <someemail>
2017-06-01 20:17:58 +0200

    besouro automatic message

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -14,13 +14,13 @@ public class StringCalculatorTest {
 	public void test_ResultWithComas(){
 		StringCalculator calculator = new StringCalculator();
 		int result= calculator.add("1,2,3");
-		assertEquals("Not valid result",6 , result);
+		assertEquals("Not valid result", 6 , result);
 	}
 	@Test
 	public void test_ResultWithNewLine(){
 		StringCalculator calculator = new StringCalculator();
 		int result= calculator.add("1\n2\n3");
-		assertEquals("Not valid result",6 , result);
+		assertEquals("Not valid result", 6 , result);
 	}
 
 }
