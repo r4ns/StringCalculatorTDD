be1b6dbc9905f6591c4b1d7770a0c36a83e9d8cc
itc3 <itc3@ws5.itc03.edu>
2017-06-02 20:50:35 +0200

    calc

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,78 @@
 
 public class StringCalculator {
+	
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
 		
 		// not yet implemented
-		return 0;
+		if(numbersStr == ""){
+			return 0;
+		}
+		String [] niz = numbersStr.split(",|\\\n");
+		
+		int result = 0;
+		for (int i = 0; i < niz.length; i++) {
+			result += Integer.parseInt(niz[i]);
+		}
+		return result;
 	}
+	
+	public int oduzimanje(String numbersStr) {
+		// Returns the sum of the numbers given in numbersStr
+		
+		// not yet implemented
+		if(numbersStr == ""){
+			return 0;
+		}
+		String [] niz = numbersStr.split(",|\\\n");
+		
+		int result = Integer.parseInt(niz[0]);
+		for (int i = 1; i < niz.length; i++) {
+			result -= Integer.parseInt(niz[i]);
+		}
+		return result;
+	}
+	
+	public int mnozenje(String numbersStr) {
+		// Returns the sum of the numbers given in numbersStr
+		
+		// not yet implemented
+		if(numbersStr == ""){
+			return 0;
+		}
+		String [] niz = numbersStr.split(",|\\\n");
+		
+		int result = 1;
+		for (int i = 0; i < niz.length; i++) {
+			result *= Integer.parseInt(niz[i]);
+		}
+		return result;
+	}
+	
+	public int deljenje(String numbersStr){
+		// Returns the sum of the numbers given in numbersStr
+		
+		// not yet implemented
+		if(numbersStr == ""){
+			return 0;
+		}
+		
+		
+		String [] niz = numbersStr.split(",|\\\n");
+		
+		for (int i = 0; i < niz.length; i++) {
+			if(Integer.parseInt(niz[i]) < 0){
+			}
+		}
+		
+		int result = Integer.parseInt(niz[0]);
+		for (int i = 1; i < niz.length; i++) {
+			result /= Integer.parseInt(niz[i]);
+		}
+		return result;
+	}
+	
+	
+	
+	
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,71 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test1(){
+		StringCalculator s = new StringCalculator();
+		assertNotNull(s);
+	}
+	
+	@Test
+	public void testAdd(){
+		StringCalculator s = new StringCalculator();
+		int result = s.add("1,2,3");
+		assertEquals("Initial result incorrect", 6 , result);
+	}
+	
+	@Test
+	public void testAdd1(){
+		StringCalculator s = new StringCalculator();
+		int result = s.add("1\n2,3");
+		assertEquals("Initial result incorrect", 6 , result);
+	}
+	
+	@Test
+	public void testAdd2(){
+		StringCalculator s = new StringCalculator();
+		int result = s.add("");
+		assertEquals("Initial result incorrect", 0 , result);
 	}
 
+	@Test
+	public void testAdd3(){
+		StringCalculator s = new StringCalculator();
+		int result = s.add("-3,2,5");
+		assertEquals("Initial result incorrect", 4 , result);
+	}
+	
+	@Test
+	public void testOduzimanje(){
+		StringCalculator s = new StringCalculator();
+		int result = s.oduzimanje("3,1");
+		assertEquals("Initial result incorrect", 2 , result);
+	}
+	
+	@Test
+	public void testOduzimanje1(){
+		StringCalculator s = new StringCalculator();
+		int result = s.oduzimanje("10,1,2,5");
+		assertEquals("Initial result incorrect", 2 , result);
+	}
+	
+	@Test
+	public void testMnozenje(){
+		StringCalculator s = new StringCalculator();
+		int result = s.mnozenje("1,3,5");
+		assertEquals("Initial result incorrect", 15 , result);
+	}
+	
+	@Test
+	public void testDeljenje(){
+		StringCalculator s = new StringCalculator();
+		int result = s.deljenje("10,2,1");
+		assertEquals("Initial result incorrect", 5 , result);
+	}
+	
+	@Test
+	public void testDeljenje1(){
+		StringCalculator s = new StringCalculator();
+		int result = s.deljenje("10,2,1");
+		assertEquals("Initial result incorrect", 5 , result);
+	}
 }
