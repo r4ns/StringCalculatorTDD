c54ea4015edced7092639d32740b53495aa6b906
somename <someemail>
2017-06-02 20:03:05 +0200

    besouro automatic message

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -11,10 +11,10 @@ public class StringCalculatorTest {
 		assertEquals(rezultat,digitron.add("1,2,3"));
 	}
 	@Test
-	public void testCharacter() throws StringCalculatorException {
+	public void testAdd2() throws StringCalculatorException {
 		StringCalculator digitron = new StringCalculator();
-		int rezultat = 6;
-		assertEquals(rezultat,digitron.add("1\n2,3"));
+		int rezultat = 8;
+		assertEquals(rezultat,digitron.add("3,2,3"));
 	}
 
 }
