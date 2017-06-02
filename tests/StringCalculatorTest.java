fe5c20eba1b623e41064f5647cd3c28c1e22a572
somename <someemail>
2017-06-02 19:57:35 +0200

    besouro automatic message

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,10 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAdd() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		int rezultat = 6;
+		assertEquals(rezultat,digitron.add("1,2,3"));
 	}
 
 }
