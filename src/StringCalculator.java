87b055cffe3a95a15344881592556a006d6b40c3
somename <someemail>
2017-06-02 20:11:56 +0200

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,8 +2,29 @@
 public class StringCalculator {
 	public int add(String numbersStr) {
 		// Returns the sum of the numbers given in numbersStr
+		int sum = 0;
+		if(numbersStr == "" ){
+			
+			return 0;
+		} else {
+			
+			String stringArray[] = numbersStr.split("\\s*,\\s*");
+			for(int i=0; i<stringArray.length; i++){
+				
+				sum = sum + Integer.parseInt(stringArray[i]);
+			}
+			
+			return sum;
+			
+			
+			
+			
+		}
+		
+		
+		
 		
 		// not yet implemented
-		return 0;
+		
 	}
 }
