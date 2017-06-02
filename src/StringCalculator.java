c1fe368a9de2b1c88144f94e69868747dc4bf968
somename <someemail>
2017-06-02 19:57:32 +0200

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,13 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
+	public int add(String numbersStr) throws StringCalculatorException {
+		String[] niz=numbersStr.split(",|\\\n");
+		int[] brojevi=new int[niz.length];
+		for(int i=0;i<numbersStr.length()-1;i++){
+			if(numbersStr.substring(i, i+2).equals(",\n")||numbersStr.substring(i,i+2).equals("\n,")){
+				throw new StringCalculatorException();
+			}
+		}
 		return 0;
 	}
 }
