989849e6519f2f5bdd402b375f8d44f503b0811f
IT13g2015 <IT13g2015@DESKTOP-VKDDF94.itc03.edu>
2017-06-02 13:07:38 +0200

    Probni test

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,83 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
+	
+	public int add(String numbersStr) throws StringCalculatorException {
+		String[] niz=numbersStr.split(",|\\\n");
+		int[] brojevi=new int[niz.length];
+		for(int i=0;i<numbersStr.length()-1;i++){
+			if(numbersStr.substring(i, i+2).equals(",\n")||numbersStr.substring(i,i+2).equals("\n,")){
+				throw new StringCalculatorException();
+			}
+		}
 		
-		// not yet implemented
-		return 0;
+		for(int i=0;i<niz.length;i++){
+			
+			brojevi[i]=Integer.parseInt(niz[i]);
+			if(brojevi[i]<0){
+				throw new StringCalculatorException();
+			}
+		}
+		int rezultat=0;
+		for(int i=0;i<brojevi.length;i++){
+			rezultat+=brojevi[i];
+		}
+		return rezultat;
 	}
+	public int oduzmi(String numbersStr) throws StringCalculatorException {
+		if(numbersStr==""){
+			return 0;
+		}
+		for(int i=0;i<numbersStr.length()-1;i++){
+			if(numbersStr.substring(i, i+2).equals(",\n")||numbersStr.substring(i,i+2).equals("\n,")){
+				throw new StringCalculatorException();
+			}
+		}
+		String[] niz=numbersStr.split(",|\\\n");
+		int[] brojevi=new int[niz.length];
+			
+			for(int i=0;i<niz.length;i++){
+				
+				brojevi[i]=Integer.parseInt(niz[i]);
+				if(brojevi[i]<0){
+					throw new StringCalculatorException();
+				}
+			}
+			int rezultat=brojevi[0];
+			for(int i=1;i<brojevi.length;i++){
+				rezultat-=brojevi[i];
+			}
+			if(rezultat<0){
+				throw new StringCalculatorException();
+			}
+			return rezultat;
+	}
+	public int pomnozi(String numbersStr) throws StringCalculatorException {
+		if(numbersStr==""){
+			return 0;
+		}
+		for(int i=0;i<numbersStr.length()-1;i++){
+			if(numbersStr.substring(i, i+2).equals(",\n")||numbersStr.substring(i,i+2).equals("\n,")){
+				throw new StringCalculatorException();
+			}
+		}
+		String[] niz=numbersStr.split(",|\\\n");
+		int[] brojevi=new int[niz.length];
+		
+			for(int i=0;i<niz.length;i++){
+				
+				brojevi[i]=Integer.parseInt(niz[i]);
+				if(brojevi[i]<0){
+					throw new StringCalculatorException();
+				}
+			}
+			int rezultat=1;
+			for(int i=1;i<brojevi.length;i++){
+				rezultat*=brojevi[i];
+			}
+			return rezultat;
+		
+		
+	}
+	
+	
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,108 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAdd() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		int rezultat = 6;
+		assertEquals(rezultat,digitron.add("1,2,3"));
+	}
+	@Test
+	public void testAddWithnCharacter() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		int rezultat = 6;
+		assertEquals(rezultat,digitron.add("1\n2,3"));
+	}
+	@Test
+	public void testAddjustOnenumber() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		int rezultat = 123;
+		assertEquals(rezultat,digitron.add("123"));
+	}
+	@Test(expected=StringCalculatorException.class)
+	public void testAddExceptionMinus() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		digitron.add("-1,2,3");
+	}
+	@Test(expected=StringCalculatorException.class)
+	public void testAddExceptionn() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		digitron.add("1,\n");
+	}
+	@Test(expected=StringCalculatorException.class)
+	public void testAddExceptionn2() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		digitron.add("1\n,");
+	}
+	@Test
+	public void testOduzmiPocetak() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		int rezultat = 0;
+		assertEquals(rezultat,digitron.oduzmi(""));
+	}
+	@Test
+	public void testOduzmiBrojeviSaZarezom() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		int rezultat = 3;
+		assertEquals(rezultat,digitron.oduzmi("5,1,1"));
+	}
+	@Test
+	public void testOduzmiBrojeviSan() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		int rezultat = 3;
+		assertEquals(rezultat,digitron.oduzmi("5\n1,1"));
+	}
+	@Test(expected=StringCalculatorException.class)
+	public void testMinusExceptionMinus() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		digitron.oduzmi("-1,2,3");
+	}
+	@Test(expected=StringCalculatorException.class)
+	public void testMinusExceptionn() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		digitron.oduzmi("1,\n");
+	}
+	@Test(expected=StringCalculatorException.class)
+	public void testMinusExceptionn2() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		digitron.oduzmi("1\n,");
+	}
+	@Test(expected=StringCalculatorException.class)
+	public void testMinusKadRezultatIdeUMinus() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		digitron.oduzmi("5,1,6");
+	}
+	@Test
+	public void testPomnozi() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		int rezultat = 0;
+		assertEquals(rezultat,digitron.pomnozi(""));
+	}
+	@Test
+	public void testPomnoziSaZarezom() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		int rezultat = 6;
+		assertEquals(rezultat,digitron.pomnozi("1,2,3"));
+	}
+	@Test
+	public void testPomnoziSaCharactern() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		int rezultat = 6;
+		assertEquals(rezultat,digitron.pomnozi("1\n2,3"));
+	}
+	@Test(expected=StringCalculatorException.class)
+	public void testPomnoziExceptionMinus() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		digitron.pomnozi("-1,2,3");
+	}
+	@Test(expected=StringCalculatorException.class)
+	public void testPomnoziExceptionn() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		digitron.pomnozi("1,\n");
+	}
+	@Test(expected=StringCalculatorException.class)
+	public void testPomnoziExceptionn2() throws StringCalculatorException {
+		StringCalculator digitron = new StringCalculator();
+		digitron.pomnozi("1\n,");
 	}
 
 }
