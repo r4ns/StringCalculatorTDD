0d5b3c8ad5dd58166c18739b22df4b91b94ba75f
SmArt User <SmArt User@DESKTOP-214PFU1>
2017-06-02 12:54:55 +0200

    StringCalculator

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,111 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
+	private String[] nizBrojeva;
+	private double rez = 0;
+	
+	public double add(String numbersStr) throws StringCalculatorException {
+		for (int i = 0; i < numbersStr.length() - 1; i++) {
+			if (numbersStr.substring(i, i+2).equals(",\n") || numbersStr.substring(i, i+2).equals("\n,")) {
+				throw new StringCalculatorException();
+			}
+		}
+		if (numbersStr != "") {
+			this.nizBrojeva = numbersStr.split(",|\\\n");
+			int[] niz = new int[nizBrojeva.length];
+			for (int i = 0; i < nizBrojeva.length; i++) {
+				niz[i] = Integer.parseInt(nizBrojeva[i]);
+			}
+			for (int i = 0; i < niz.length; i++) {
+				if (niz[i] <= 0) {
+					throw new StringCalculatorException();
+				}
+				rez = rez + niz[i];
+			}
+			return rez;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	public double oduzmi (String numbersStr) throws StringCalculatorException {
+		for (int i = 0; i < numbersStr.length() - 1; i++) {
+			if (numbersStr.substring(i, i+2).equals(",\n") || numbersStr.substring(i, i+2).equals("\n,")) {
+				throw new StringCalculatorException();
+			}
+		}
+		if (numbersStr != "") {
+			this.nizBrojeva = numbersStr.split(",|\\\n");
+			int[] niz = new int[nizBrojeva.length];
+			for (int i = 0; i < nizBrojeva.length; i++) {
+				niz[i] = Integer.parseInt(nizBrojeva[i]);
+			}
+			rez = niz[0];
+			for (int i = 1; i < niz.length; i++) {
+				if (niz[i] <= 0 || niz[0] <= 0) {
+					throw new StringCalculatorException();
+				}
+				else if (niz[i] > niz[i-1]) {
+					throw new StringCalculatorException();
+				}
+				rez = rez - niz[i];
+			}
+			return rez;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	public double podeli (String numbersStr) throws StringCalculatorException {
+		for (int i = 0; i < numbersStr.length() - 1; i++) {
+			if (numbersStr.substring(i, i+2).equals(",\n") || numbersStr.substring(i, i+2).equals("\n,")) {
+				throw new StringCalculatorException();
+			}
+		}
+		if (numbersStr != "") {
+			this.nizBrojeva = numbersStr.split(",|\\\n");
+			int[] niz = new int[nizBrojeva.length];
+			for (int i = 0; i < nizBrojeva.length; i++) {
+				niz[i] = Integer.parseInt(nizBrojeva[i]);
+			}
+			rez = niz[0];
+			for (int i = 1; i < niz.length; i++) {
+				if (niz[i] <= 0 || niz[0] <= 0) {
+					throw new StringCalculatorException();
+				}
+				rez = rez / niz[i];
+			}
+			return rez;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	public double pomnozi(String numbersStr) throws StringCalculatorException {
+		rez = 1;
+		for (int i = 0; i < numbersStr.length() - 1; i++) {
+			if (numbersStr.substring(i, i+2).equals(",\n") || numbersStr.substring(i, i+2).equals("\n,")) {
+				throw new StringCalculatorException();
+			}
+		}
+		if (numbersStr != "") {
+			this.nizBrojeva = numbersStr.split(",|\\\n");
+			int[] niz = new int[nizBrojeva.length];
+			for (int i = 0; i < nizBrojeva.length; i++) {
+				niz[i] = Integer.parseInt(nizBrojeva[i]);
+			}
+			for (int i = 0; i < niz.length; i++) {
+				if (niz[i] <= 0) {
+					throw new StringCalculatorException();
+				}
+				rez = rez * niz[i];
+			}
+			return rez;
+		}
+		else {
+			return 0;
+		}
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -3,10 +3,110 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-
+	
+	StringCalculator test = new StringCalculator();
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_initialAdd() throws StringCalculatorException {
+		int rezultat = 1;
+		assertEquals(rezultat, test.add("1"));
 	}
-
+	@Test
+	public void test_initialAdd_Zero() throws StringCalculatorException {
+		int rezultat = 0;
+		assertEquals(rezultat, test.add(""));
+	}
+	
+	@Test
+	public void test_initialsAdd() throws StringCalculatorException {
+		int rezultat = 6;
+		assertEquals(rezultat, test.add("1,2,3"));
+	}
+	
+	@Test
+	public void test_initialsAdd_newLine() throws StringCalculatorException {
+		int rezultat = 6;
+		assertEquals(rezultat, test.add("1\n2,3"));
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void test_initialsAdd_newLine_comma() throws StringCalculatorException {
+		test.add(",\n");
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void test_initialAdd_Negative() throws StringCalculatorException {
+		test.add("-1");
+	}
+	
+	@Test
+	public void test_initialOduzmi() throws StringCalculatorException {
+		int rezultat = 1;
+		assertEquals(rezultat, test.oduzmi("1"));
+	}
+	@Test
+	public void test_initialOduzmi_Zero() throws StringCalculatorException {
+		int rezultat = 0;
+		assertEquals(rezultat, test.oduzmi(""));
+	}
+	
+	@Test
+	public void test_initialsOduzmi() throws StringCalculatorException {
+		int rezultat = 0;
+		assertEquals(rezultat, test.oduzmi("3,2,1"));
+	}
+	
+	@Test
+	public void test_initialsOduzmi_newLine() throws StringCalculatorException {
+		int rezultat = 0;
+		assertEquals(rezultat, test.oduzmi("3\n2,1"));
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void test_initialsOduzmi_newLine_comma() throws StringCalculatorException {
+		test.oduzmi(",\n");
+	}
+	
+	/*@Test(expected = StringCalculatorException.class)
+	public void test_initialOduzmi_Negative() throws StringCalculatorException {
+		test.oduzmi("-1");
+	}*/
+	
+	@Test(expected = StringCalculatorException.class)
+	public void test_initialsOduzmi_Exc() throws StringCalculatorException {
+		test.oduzmi("2,3,1");
+	}
+	
+	@Test
+	public void test_initialPomnozi() throws StringCalculatorException {
+		int rezultat = 1;
+		assertEquals(rezultat, test.pomnozi("1"));
+	}
+	@Test
+	public void test_initialPomnozi_Zero() throws StringCalculatorException {
+		int rezultat = 0;
+		assertEquals(rezultat, test.pomnozi(""));
+	}
+	
+	@Test
+	public void test_initialsPomnozi() throws StringCalculatorException {
+		int rezultat = 6;
+		assertEquals(rezultat, test.pomnozi("1,2,3"));
+	}
+	
+	@Test
+	public void test_initialsPomnozi_newLine() throws StringCalculatorException {
+		int rezultat = 6;
+		assertEquals(rezultat, test.pomnozi("1\n2,3"));
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void test_initialsPomnozi_newLine_comma() throws StringCalculatorException {
+		test.pomnozi(",\n");
+	}
+	
+	@Test(expected = StringCalculatorException.class)
+	public void test_initialPomnozi_Negative() throws StringCalculatorException {
+		test.pomnozi("-1");
+	}
 }
