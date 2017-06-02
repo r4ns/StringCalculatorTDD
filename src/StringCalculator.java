eec139c39f454116856828cee4bf289255ac1c85
it47g2015 <it47g2015@ws6.itc03.edu>
2017-06-02 12:58:53 +0200

    idemooo probni test

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,48 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		
+	public double add(String numbersStr) {
+		String s[] = numbersStr.split(",");
+		String znak=s[0];
+		double suma;
+		switch(znak){
+			case "+":
+				suma=0;
+				break;
+			case "-":
+				suma=Integer.parseInt(s[1]);
+				break;
+			case "*":
+				suma=1;
+				break;
+			case "/":
+				suma=Integer.parseInt(s[1]);
+				break;
+			default:
+				suma=0;
+		}
+		switch(znak){
+		case "+":
+			for(int i=1;i<s.length;i++){
+				suma+=Double.parseDouble(s[i]);
+			}
+			break;
+		case "-":
+			for(int i=2;i<s.length;i++){
+				suma-=Double.parseDouble(s[i]);
+			}
+			break;
+		case "/":
+			for(int i=2;i<s.length;i++){
+				suma/=Double.parseDouble(s[i]);
+			}
+			break;
+		case "*":
+			for(int i=1;i<s.length;i++){
+				suma*=Double.parseDouble(s[i]);
+			}
+			break;
+		}
 		// not yet implemented
-		return 0;
+		return suma;
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -2,11 +2,39 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
+
 public class StringCalculatorTest {
+	private static final double DELTA = 1e-15;
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testAdd() {
+		StringCalculator s = new StringCalculator();
+		assertEquals(15.0,s.add("+,1,2,3,4,5"),DELTA);
+	}
+	
+	@Test
+	public void testMultiple() {
+		StringCalculator s = new StringCalculator();
+		assertEquals(120,s.add("*,1,2,3,4,5"),DELTA);
+	}
+	
+	@Test
+	public void testDivide() {
+		StringCalculator s = new StringCalculator();
+		assertEquals(0.0083333,s.add("/,1,2,3,4,5"),DELTA);
+	}
+	
+	@Test
+	public void testSub() {
+		StringCalculator s = new StringCalculator();
+		assertEquals(-15,s.add("-,1,2,3,4,5"),DELTA);
+	}
+	
+	@Test
+	public void testConstructor(){
+		StringCalculator s = new StringCalculator();
+		assertNotNull(s);
 	}
 
 }
