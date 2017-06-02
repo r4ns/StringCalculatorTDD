0178a9b43eac2ab0c39a3c892b4cd35f816bbf10
it56g2015 <it56g2015@ws11.itc03.edu>
2017-06-02 13:06:36 +0200

    it56

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,47 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
+	public int add(String numbersStr) throws StringCalculatorException {
+		int suma = 0;
+		if(numbersStr.length() == 0){
+			return 0;
+		}
+		String brojevi[] = numbersStr.split("[\n,]");
+		int duzina = brojevi.length;
+		for(int i = 0; i < duzina;i++){
+			if(duzina == 1){
+				throw new StringCalculatorException();
+			}
+			int pomocna = Integer.parseInt(brojevi[i]);
+			if(pomocna <= 0){
+				throw new StringCalculatorException();
+			}
+			suma += pomocna;
+		}
+		return suma;
+	}
+
+	public int substract(String numbersStr) throws StringCalculatorException {
+		int rez = 0;
+		if(numbersStr.length() == 0){
+			return 0;
+		}
+		String brojevi[] = numbersStr.split("[\n,]");
+		int duzina = brojevi.length;
+
+		for(int i = 0; i < duzina;i++){
+			if(duzina == 1){
+				throw new StringCalculatorException();
+			}
+			if(Integer.parseInt(brojevi[i]) < Integer.parseInt(brojevi[i+1])){
+				throw new StringCalculatorException();
+			}
+			int pomocna = Integer.parseInt(brojevi[i]);
+			if(pomocna <= 0){
+				throw new StringCalculatorException();
+			}
+			rez -= pomocna;
+		}
+		return rez;
 	}
 }
+
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -4,9 +4,75 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_empty_input() throws StringCalculatorException{
+		StringCalculator sc = new StringCalculator();
+		int rez = sc.add("");
+		
+		assertEquals("Greska!", 0, rez);
 	}
-
+	
+	@Test
+	public void test_sum_1_1() throws StringCalculatorException{
+		StringCalculator sc = new StringCalculator();
+		int rez = sc.add("1,1");
+		
+		assertEquals("Greska!", 2, rez);
+	}
+	
+	@Test
+	public void test_sum_4_6() throws StringCalculatorException{
+		StringCalculator sc = new StringCalculator();
+		int rez = sc.add("4,6");
+		
+		assertEquals("Greska!", 10, rez);
+	}
+	
+	@Test
+	public void test_sum_4_6_9() throws StringCalculatorException{
+		StringCalculator sc = new StringCalculator();
+		int rez = sc.add("4,6,9");
+		
+		assertEquals("Greska!", 19, rez);
+	}
+	@Test
+	public void test_sum_4_n6_9() throws StringCalculatorException{
+		StringCalculator sc = new StringCalculator();
+		int rez = sc.add("4\n6,9");
+		
+		assertEquals("Greska!", 19, rez);
+	}
+	@Test
+	public void test_sum_4_n() throws StringCalculatorException{
+		StringCalculator sc = new StringCalculator();
+		int rez = sc.add("4\n6");
+		
+		assertEquals("Greska!", 10, rez);
+	}
+	
+	@Test
+	public void test_multi_2_2() throws StringCalculatorException{
+		StringCalculator sc = new StringCalculator();
+		int rez = sc.add("4\n6,9");
+		
+		assertEquals("Greska!", 19, rez);
+	}
+	@Test(expected = StringCalculatorException.class)
+	public void test_1_comma_newline() throws StringCalculatorException{
+		StringCalculator sc = new StringCalculator();
+		int rez = sc.add("2,\n");
+	}
+	
+	@Test
+	public void substract_2_2() throws StringCalculatorException{
+		StringCalculator sc = new StringCalculator();
+		int rez = sc.substract("2,1");
+		
+		assertEquals("Greska!", 1, rez);
+	}
 }
