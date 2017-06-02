3e730776d16511e7e89fb526dc171371611526df
somename <someemail>
2017-06-02 20:24:05 +0200

    besouro automatic message

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -22,12 +22,12 @@ public class StringCalculatorTest {
 		int rezultat = 6;
 		assertEquals(rezultat,digitron.add("1\n2,3"));
 	}
-	/*@Test
+	@Test
 	public void testSpace() throws StringCalculatorException {
 		StringCalculator digitron = new StringCalculator();
 		int rezultat = 0;
 		assertEquals(rezultat,digitron.add(""));
-	}*/
+	}
 	@Test
 	public void testAddjustOnenumber() throws StringCalculatorException {
 		StringCalculator digitron = new StringCalculator();
