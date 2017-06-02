3d0ffb2bb6fe465928b55b6dfcb20bfb3b880c9a
it43g2015 <it43g2015@WS3.itc03.edu>
2017-06-02 12:58:54 +0200

    Fali deljenje

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,68 @@
 
 public class StringCalculator {
+	
+	String numbersStr;
+	public StringCalculator(String numbersStr){
+		this.numbersStr = numbersStr;
+	}
+	
+	
+	
 	public int add(String numbersStr) {
+		
+		String[] unos;
+		int[] unosInt = null;
+		int rezultat = 0;
+		unos = numbersStr.split(",");
+		for(int i = 0; i<unos.length; i++){
+			rezultat += Integer.parseInt(unos[i]);
+			//unosInt[i] = Integer.parseInt(unos[i]);
+			
+		}
+				
 		// Returns the sum of the numbers given in numbersStr
 		
 		// not yet implemented
-		return 0;
+		return rezultat;
 	}
+	
+	
+	public int substract(String numbersStr) {
+		
+		String[] unos;
+		int[] unosInt = null;
+		int rezultat = 0;
+		unos = numbersStr.split(",");
+		for(int i = 0; i<unos.length; i++){
+			rezultat = Integer.parseInt(unos[0]);
+			rezultat -= Integer.parseInt(unos[i]);
+			//unosInt[i] = Integer.parseInt(unos[i]);
+			
+		}
+				
+		// Returns the sum of the numbers given in numbersStr
+		
+		// not yet implemented
+		return rezultat;
+	}
+	
+
+	public int multiply(String numbersStr) {
+		
+		String[] unos;
+		int[] unosInt = null;
+		int rezultat = 1;
+		unos = numbersStr.split(",");
+		for(int i = 0; i<unos.length; i++){
+			rezultat *= Integer.parseInt(unos[i]);
+			//unosInt[i] = Integer.parseInt(unos[i]);
+			
+		}
+				
+		// Returns the sum of the numbers given in numbersStr
+		
+		// not yet implemented
+		return rezultat;
+	}
+	
 }
