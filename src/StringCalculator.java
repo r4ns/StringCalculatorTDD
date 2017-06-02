0df87c17a2b2510b50d471957131ecb83fe87b90
somename <someemail>
2017-06-02 20:01:43 +0200

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -2,22 +2,26 @@
 public class StringCalculator {
 	public int add(String numbersStr) throws StringCalculatorException {
 		String[] niz=numbersStr.split(",|\\\n");
-		int[] brojevi=new int[niz.length];
-		for(int i=0;i<numbersStr.length()-1;i++){
-			if(numbersStr.substring(i, i+2).equals(",\n")||numbersStr.substring(i,i+2).equals("\n,")){
+		int[] numbers=new int[niz.length];
+		
+		for(int i=0;i<numbersStr.length()-1;i++)
+		{
+			
+			if(numbersStr.substring(i, i+2).equals(",\n")||numbersStr.substring(i,i+2).equals("\n,"))
+			{
 				throw new StringCalculatorException();
 			}
 		}
 			for(int i=0;i<niz.length;i++){
 			
-			brojevi[i]=Integer.parseInt(niz[i]);
-			if(brojevi[i]<0){
+			numbers[i]=Integer.parseInt(niz[i]);
+			if(numbers[i]<0){
 				throw new StringCalculatorException();
 			}
 		}
 		int rezultat=0;
-		for(int i=0;i<brojevi.length;i++){
-			rezultat+=brojevi[i];
+		for(int i=0;i<numbers.length;i++){
+			rezultat+=numbers[i];
 		}
 		return rezultat;
 	}
