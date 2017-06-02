8cdd0e244725b213becb94dbbeb71454fbb5f736
MOCKO <MOCKO@DESKTOP-SBTP6EU>
2017-06-02 21:48:30 +0200

    Kalkukator TD

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -5,49 +5,88 @@ public class StringCalculator {
 	}
 	
 	public int add(String numbersStr)throws StringCalculatorException {
-		String [] niz = numbersStr.split(",");
-		String znak = niz[0];
+		checkValidString(numbersStr);
+		String delimiter1 = ",";
+		String delimiter2 = "\n";
+		
+		numbersStr = numbersStr.replaceAll(delimiter1,delimiter2);
+		String [] niz1 = numbersStr.split(delimiter2);
+		int [] niz = new int[niz1.length];
+		
+		for(int i=1;i<niz.length;i++){
+			niz[i]= Integer.parseInt(niz1[i]);
+		}
+		
+		char znak = niz1[0].charAt(0);
+		
+		checkNumbers(niz);
+		
 		int rezultat=0;
-		//int razlika=0;
-		switch(znak){
-		case "+": 
-			for(int i=1;i<niz.length;i++){
-				rezultat += Integer.parseInt(niz[i]);
-			}
-			break;
 		
-		case "-":
-			for(int i=1;i<niz.length;i++){
-				if(i == 1)
-					rezultat = Integer.parseInt(niz[i]);
-				else
-					rezultat -= Integer.parseInt(niz[i]);
-			}
-			break;
+		if(znak == ' ')
+			return Integer.valueOf(rezultat);
 			
-		case "*":
+		if(znak == '+') 
+			for(int i=1;i<niz.length;i++)
+					rezultat += niz[i];
+			
+		if(znak =='-')
 			for(int i=1;i<niz.length;i++){
 				if(i == 1)
-					rezultat = Integer.parseInt(niz[i]);
+					rezultat = niz[i];
 				else
-					rezultat *= Integer.parseInt(niz[i]);
+					rezultat -= niz[i];
 			}
-			break;
-		
-		
-		case "/":
+		if(znak == '*')
 			for(int i=1;i<niz.length;i++){
-				if(Integer.parseInt(niz[i])==0){
+				if(i == 1)
+					rezultat = niz[i];
+				else
+					rezultat *= niz[i];
+			}
+		if(znak == '/')
+			for(int i=1;i<niz.length;i++){
+				if(niz[i]==0){
 					  throw new StringCalculatorException();
 				}
 				else if(i == 1)
-					rezultat = Integer.parseInt(niz[i]);
+					rezultat =niz[i];
 				else
-					rezultat /= Integer.parseInt(niz[i]);
+					rezultat /= niz[i];
 			}
-			break;
-	
-		}
 		return Integer.valueOf(rezultat);
 	}
+	
+	public void checkValidString(String numbersStr)throws StringCalculatorException {
+		
+		char [] provera = numbersStr.toCharArray();
+		char [] karakteri = {'0','1','2','3','4','5','6','7','8','9','+','-','*','/',',',' ','\n'};
+		int count=0;
+		for(int i=0;i<numbersStr.length();i++){
+			
+			if(provera[i]==',' && provera[i+1]=='\n')
+				throw new StringCalculatorException();
+			
+			if(provera[i]=='\n' && provera[i+1]==',')
+				throw new StringCalculatorException();
+			
+			if(provera[i]!='+' && provera[i]!='-' && provera[i]!='*' && provera[i]!='/' && provera[i] !=' ' && Integer.valueOf(provera[i])<0)
+				throw new StringCalculatorException();
+			
+			for(int j=0;j<karakteri.length;j++){
+				if(Integer.valueOf(provera[i])==karakteri[j])
+					count++;
+			}
+		}
+		if(count!=numbersStr.length())
+			throw new StringCalculatorException();
+	}
+	
+	public void checkNumbers(int [] niz)throws StringCalculatorException{
+		for(int i=0;i<niz.length;i++){
+			if(!(niz[i]>=0))
+				throw new StringCalculatorException();
+		}
+	}
+	
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -13,8 +13,8 @@ public class StringCalculatorTest {
 	public void test_add()throws StringCalculatorException {
 		String array= "+,1,2,3,4,5";
 		StringCalculator s = new StringCalculator();
-		String expected = "15";
 		
+		String expected = "15";
 		assertEquals(expected,String.valueOf(s.add(array)));
 	}
 	
@@ -41,7 +41,6 @@ public class StringCalculatorTest {
 		
 		assertEquals(expected,String.valueOf(s.add(array)));
 	}
-	
 	
 	@Test 
 	public void test_multiplication_more_element()throws StringCalculatorException{
@@ -70,4 +69,68 @@ public class StringCalculatorTest {
 		assertEquals(expected,String.valueOf(s.add(array)));
 	}
 	
+	@Test 
+	public void test_empty_string()throws StringCalculatorException{
+		String array= " ";
+		StringCalculator s = new StringCalculator();
+		
+		String expected = "0";
+		assertEquals(expected, String.valueOf(s.add(array)));
+		
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void test_n_is_not_valid()throws StringCalculatorException{
+		String array = "1,2,3,\n,4,5";
+		StringCalculator s = new StringCalculator();
+		
+		String expected = "exception";
+		assertEquals(expected, String.valueOf(s.add(array)));
+		
+	}
+	
+	@Test
+	public void test_n_is_valid_for_add()throws StringCalculatorException{
+		String array = "+,1,2,3\n4,5";
+		StringCalculator s = new StringCalculator();
+		
+		String expected = "15";
+		assertEquals(expected, String.valueOf(s.add(array)));
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void test_n_2_is_not_valid()throws StringCalculatorException{
+		String array = "1,2,3,\n,4,5";
+		StringCalculator s = new StringCalculator();
+		
+		String expected = "exception";
+		assertEquals(expected, String.valueOf(s.add(array)));
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void invalid_input_with_negative_number()throws StringCalculatorException{
+		String array = "+,1,2,3,-4,5";
+		StringCalculator s = new StringCalculator();
+		
+		String expected = "exception";
+		assertEquals(expected, String.valueOf(s.add(array)));
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void invalid_input_with_some_character()throws StringCalculatorException{
+		String array = "-,1,2,?,14,5,6";
+		StringCalculator s = new StringCalculator();
+		
+		String expected = "exception";
+		assertEquals(expected, String.valueOf(s.add(array)));
+	}
+	/*
+	@Test()
+	public void test_with_more_operand()throws StringCalculatorException{
+		String array = "-,1,2,+,1,5,6";
+		StringCalculator s = new StringCalculator();
+		
+		String expected = "11";
+		assertEquals(expected, String.valueOf(s.add(array)));
+	}*/
 }
