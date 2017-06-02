bbe54e0a6dd5e53e724cd04d7728ccbf4981f68a
it54g2015 <it54g2015@ws4.itc03.edu>
2017-06-02 13:05:59 +0200

    Kalkulator

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,53 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
+	public StringCalculator() {
 		
-		// not yet implemented
-		return 0;
+	}
+	
+	public int add(String numbersStr)throws StringCalculatorException {
+		String [] niz = numbersStr.split(",");
+		String znak = niz[0];
+		int rezultat=0;
+		//int razlika=0;
+		switch(znak){
+		case "+": 
+			for(int i=1;i<niz.length;i++){
+				rezultat += Integer.parseInt(niz[i]);
+			}
+			break;
+		
+		case "-":
+			for(int i=1;i<niz.length;i++){
+				if(i == 1)
+					rezultat = Integer.parseInt(niz[i]);
+				else
+					rezultat -= Integer.parseInt(niz[i]);
+			}
+			break;
+			
+		case "*":
+			for(int i=1;i<niz.length;i++){
+				if(i == 1)
+					rezultat = Integer.parseInt(niz[i]);
+				else
+					rezultat *= Integer.parseInt(niz[i]);
+			}
+			break;
+		
+		
+		case "/":
+			for(int i=1;i<niz.length;i++){
+				if(Integer.parseInt(niz[i])==0){
+					  throw new StringCalculatorException();
+				}
+				else if(i == 1)
+					rezultat = Integer.parseInt(niz[i]);
+				else
+					rezultat /= Integer.parseInt(niz[i]);
+			}
+			break;
+	
+		}
+		return Integer.valueOf(rezultat);
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -3,10 +3,71 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-
+/*
 	@Test
 	public void test() {
+		
 		fail("Not yet implemented");
+	}*/
+	@Test
+	public void test_add()throws StringCalculatorException {
+		String array= "+,1,2,3,4,5";
+		StringCalculator s = new StringCalculator();
+		String expected = "15";
+		
+		assertEquals(expected,String.valueOf(s.add(array)));
 	}
-
+	
+	@Test
+	public void test_constructor(){
+		StringCalculator s = new StringCalculator();
+		assertNotNull(s);
+	}
+	
+	@Test 
+	public void test_substraction_2_element()throws StringCalculatorException{
+		String array= "-,5,4";
+		StringCalculator s = new StringCalculator();
+		String expected = "1";
+		
+		assertEquals(expected,String.valueOf(s.add(array)));
+	}
+	
+	@Test 
+	public void test_substraction_more_element()throws StringCalculatorException{
+		String array= "-,5,4,3,2,1";
+		StringCalculator s = new StringCalculator();
+		String expected = "-5";
+		
+		assertEquals(expected,String.valueOf(s.add(array)));
+	}
+	
+	
+	@Test 
+	public void test_multiplication_more_element()throws StringCalculatorException{
+		String array= "*,5,4";
+		StringCalculator s = new StringCalculator();
+		String expected = "20";
+		
+		assertEquals(expected,String.valueOf(s.add(array)));
+	}
+	
+	@Test 
+	public void test_division_more_element()throws StringCalculatorException{
+		String array= "/,8,4";
+		StringCalculator s = new StringCalculator();
+		String expected = "2";
+		
+		assertEquals(expected,String.valueOf(s.add(array)));
+	}
+	
+	@Test(expected=StringCalculatorException.class) 
+	public void test_division_more_element_exception()throws StringCalculatorException{
+		String array= "/,8,4,0";
+		StringCalculator s = new StringCalculator();
+		String expected = "2";
+		
+		assertEquals(expected,String.valueOf(s.add(array)));
+	}
+	
 }
