65ced41a24d489c95b376ace1a7a8a9e2357edd5
somename <someemail>
2017-06-02 20:23:01 +0200

    besouro automatic message

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -21,12 +21,42 @@ public class StringCalculator {
 				throw new StringCalculatorException("Nije moguce vrsiti  datu operaciju sa negativnim brojevima");
 			}
 			
+			
 		}
 		int rezultat=0;
 		for(int i=0;i<numbers.length;i++){
 			rezultat+=numbers[i];
 		}
 		return rezultat;
+	}
+	public int oduzimanje(String numbersStr) throws StringCalculatorException {
+		if(numbersStr==""){
+			return 0;
+		}
+		for(int i=0;i<numbersStr.length()-1;i++){
+			if(numbersStr.substring(i, i+2).equals(",\n")||numbersStr.substring(i,i+2).equals("\n,")){
+				throw new StringCalculatorException("Greska");
+			}
+		}
+		String[] niz=numbersStr.split(",|\\\n");
+		int[] brojevi=new int[niz.length];
+			
+			for(int i=0;i<niz.length;i++){
+				
+				brojevi[i]=Integer.parseInt(niz[i]);
+				if(brojevi[i]<0){
+					throw new StringCalculatorException("Negativan broj");
+				}
+			}
+			int rezultat=brojevi[0];
+			for(int i=1;i<brojevi.length;i++){
+				rezultat-=brojevi[i];
+			}
+			if(rezultat<0){
+				throw new StringCalculatorException("Rezultat je negativan");
+			}
+			return rezultat;
+		
 	}
 	
 }
