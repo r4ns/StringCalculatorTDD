4c218665ff630e8b136cbda1442d02b1662e7fd9
ITC11 <ITC11@ws14.itc03.edu>
2017-06-02 20:55:25 +0200

    zavrsiti

--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -5,8 +5,44 @@ import org.junit.Test;
 public class StringCalculatorTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test() throws StringCalculatorException {
+		
+		StringCalculator calc = new StringCalculator();
+		assertEquals(0,calc.add(""));
+	}
+	
+	@Test
+	public void test2() throws StringCalculatorException {
+		
+		StringCalculator calc = new StringCalculator();
+		assertEquals(10,calc.add("5,5"));
+	}
+	@Test
+	public void test3() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(10, calc.add("1\n5,4"));
+		
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void test4() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		calc.add("-5,1");
+		
+	}
+	
+	@Test
+	public void test5() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(1, calc.add("1"));
+		
+	}
+	
+	@Test
+	public void test6() throws StringCalculatorException
+	{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(10, calc.subtraction("20,10"));
 	}
 
 }
