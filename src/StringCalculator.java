38ea1a53ebd5afd6b8b4b3d75ce0bd5ca5c5c940
Home <Home@DESKTOP-P6NL2RI>
2017-06-07 23:50:02 +0200

    kraj

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,18 +1,159 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+	public int add(String numbersStr) throws StringCalculatorException {
 		// Returns the sum of the numbers given in numbersStr
 		
 		// not yet implemented
-		return 0;
+		int rezultat=0;
+		String  brojevi [] =numbersStr.split(",|\\\n");
+		if (numbersStr=="")
+		{
+			throw new StringCalculatorException("Prazan string");
+		}
+		
+		for(int i=0; i<brojevi.length; i++)
+		{
+			if (Integer.parseInt(brojevi[i]) < 0)
+			{
+				throw new StringCalculatorException("Broj je negativan");
+			}
+			else
+			{
+				rezultat+=Integer.parseInt(brojevi[i]);
+			}
+			
+			
+			
+		}
+		if(rezultat==Integer.parseInt(brojevi[0]))
+		{
+			throw new StringCalculatorException("Input nije validan");
+		}
+		
+		
+		return rezultat;
+		
+		
+		//return 0;
 	}
 	public StringCalculator()
 	{
 		
 	}
 	
+	public int  pomnozi(String numbersStr) throws StringCalculatorException
+	{
+		int rezultat=1;
+		
+		String  brojevi [] =numbersStr.split(",|\\\n");
+		
+		if (numbersStr=="")
+		{
+			throw new StringCalculatorException("Prazan string");
+			
+		}
+		
+		for(int i=0; i<brojevi.length; i++)
+		{
+			if (Integer.parseInt(brojevi[0]) < 0)
+			{
+				throw new StringCalculatorException("Negativan broj");
+			}
+			else
+			{
+				rezultat*=Integer.parseInt(brojevi[i]);
+			}
+			
+		}
+		
+		if(Integer.parseInt(brojevi[0])==rezultat)
+		{
+			throw new StringCalculatorException("Input je nevalidan");
+			
+		}
+		
+		
+		
+		return rezultat;
+		
+		
+	}
 	
-	public String saberi(String str)
+	public int  oduzimanje(String numbersStr) throws StringCalculatorException
+	{
+		
+		int rezultat=0;
+		String  brojevi [] =numbersStr.split(",|\\\n");
+		if (numbersStr=="")
+		{
+			throw new StringCalculatorException("Prazan string");
+		}
+		
+		for(int i=0; i<brojevi.length; i++)
+		{
+			if (i==0)
+			{
+				rezultat=Integer.parseInt(brojevi[0]);
+			} else
+			{
+				rezultat-=Integer.parseInt(brojevi[i]);
+			}
+			
+		}
+		
+		if(rezultat==Integer.parseInt(brojevi[0]))
+		{
+			throw new StringCalculatorException("Input nije validan");
+		}
+		
+		
+		return rezultat;
+			
+	}
+	
+	public float  deljenje(String numbersStr) throws StringCalculatorException
+	{
+		float rezultat=0;
+		
+		String  brojevi [] =numbersStr.split(",|\\\n");
+		
+		if (numbersStr=="")
+		{
+			throw new StringCalculatorException("Prazan string");
+		}
+		
+		for(int i=0; i<brojevi.length; i++)
+		{
+			
+			if (i==0)
+			{
+				rezultat=Float.parseFloat(brojevi[0]);
+			}
+			else{
+				
+				rezultat=rezultat/Float.parseFloat(brojevi[i]);
+			}
+			
+		}
+		
+		
+		if(rezultat==Integer.parseInt(brojevi[0]))
+		{
+			throw new StringCalculatorException("Input nije validan");
+		}
+		
+		if (numbersStr.contains("0"))
+		{
+			throw new StringCalculatorException("Deljenje sa 0 nije dozvoljeno");
+		}
+		
+		return rezultat;
+		
+	}
+	
+	
+	
+	/*public String saberi(String str)
 	{
 		int rezultat=0;
 		String brojevi[]=str.split(",");
@@ -38,5 +179,5 @@ public class StringCalculator {
 		String rez= Integer.toString(rezultat);
 		return rez;
 		
-	}
+	}*/
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -4,64 +4,183 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	
 
-	@Test 
-	public void testiranjeKonstruktor()
-	{
-		StringCalculator kalkulator= new StringCalculator();
-		
-		assertNotNull(kalkulator);
-	}
-	
-	@Test
-	public void testSaberiSplitNaZarez()
-	{
-		StringCalculator kalkulator= new StringCalculator();
-		
-		
-		String rezultat=kalkulator.saberi("2,3");
-		
-		assertEquals("5", rezultat);
-	
-	}
-	
-	@Test
-	public void testSaberiSakosaCrtaN()
-	{
-		StringCalculator kalkulator= new StringCalculator();
-		
-		
-		String rezultat=kalkulator.saberi("1\n2,3");
-		
-		assertEquals("6", rezultat);
-	
-	}
 	
 	@Test 
-	public void testSaberiTribroja()
+	public void testSaberiDvaBrojaBezKosaCrtaNKodSabiranja() throws StringCalculatorException
 	{
 		StringCalculator kalkulator= new StringCalculator();
-		
-		
-		String rezultat=kalkulator.saberi("2,3,3");
-		
-		assertEquals("8", rezultat);
+		String numbersStr="1,2,3,4";
+		int zbir=kalkulator.add(numbersStr);
+		assertEquals(10,zbir);
 	}
 	
 	@Test
-	public void testSaberiSakosaCrtaNSaTriBroja()
+	public void testSaberiDvaBrojaSaKosaCrtaNKodSabiranja() throws StringCalculatorException
 	{
 		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="1\n2,3";
+		int zbir=kalkulator.add(numbersStr);
+		assertEquals(6,zbir);
+	}
+	
+	public void testSaberiSaKosomCrtomNaDrugomMestuKodSabiranja() throws StringCalculatorException
+	{
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="1,2\n3";
+		int zbir=kalkulator.add(numbersStr);
+		assertEquals(6,zbir);
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void testExceptionKosaCrtaNaLosemMestuKodSabiranja() throws StringCalculatorException{
+		
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="1,\n";
+		int zbir=kalkulator.add(numbersStr);
 		
 		
-		String rezultat=kalkulator.saberi("1\n2,3,4");
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void testNegativanBrojKodSabiranja() throws StringCalculatorException{
 		
-		assertEquals("10", rezultat);
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="1,-2,3";
+		int zbir=kalkulator.add(numbersStr);
+		
+		
+	}
+	
+	
+	@Test(expected=StringCalculatorException.class)
+	public void testPrazanStringKodSabiranja() throws StringCalculatorException{
+		
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="";
+		int zbir=kalkulator.add(numbersStr);
+	}
+	
+	@Test
+	public void testPomnoziBrojeve() throws StringCalculatorException{
+		
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="1,2,3";
+		int proizvod=kalkulator.pomnozi(numbersStr);
+		assertEquals(6, proizvod);
+	}
+	
+	@Test
+	public void testPomnoziBrojeveSaKosomCrtom() throws StringCalculatorException{
+		
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="1\n2,3";
+		int proizvod=kalkulator.pomnozi(numbersStr);
+		assertEquals(6, proizvod);
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void testPrazanStringKodMnozenja() throws StringCalculatorException{
+		
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="";
+		int proizvod=kalkulator.pomnozi(numbersStr);
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void testNegativanBrojKodMnozenja() throws StringCalculatorException{
+		
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="-1,2,3";
+		int proizvod=kalkulator.pomnozi(numbersStr);
+		
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void testKodMnozenjaSaKosomCrtomNaLosemMestu() throws StringCalculatorException{
+		
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="3,\n";
+		int proizvod=kalkulator.pomnozi(numbersStr);
+		
+	}
+	
+	
+	@Test
+	public void testOduzimanje () throws StringCalculatorException
+	{
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="5,2,3";
+		
+		int razlika=kalkulator.oduzimanje(numbersStr);
+		assertEquals(0, razlika);
+	}
+	
+	@Test
+	public void testOduzimanjeSaKosomCrtom () throws StringCalculatorException
+	{
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="5\n2,3";
+		
+		int razlika=kalkulator.oduzimanje(numbersStr);
+		assertEquals(0, razlika);
+	}
+	
+	
+	@Test(expected=StringCalculatorException.class)
+	public void testPrazanStringKodOduzimanja () throws StringCalculatorException{
+		
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="";
+		int razlika=kalkulator.oduzimanje(numbersStr);
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void testKodOduzimanjaSaKosomCrtomNaLosemMestu() throws StringCalculatorException{
+		
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="3,\n";
+		int razlika=kalkulator.oduzimanje(numbersStr);
+		
+	}
+	
+	@Test
+	public void testDeljenje () throws StringCalculatorException
+	{
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="6,2";
+		
+		float kolicnik=kalkulator.deljenje(numbersStr);
+		assertEquals(3, kolicnik, 0.9);
 	
 	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void testPrazanStringKodDeljenja() throws StringCalculatorException{
+		
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="";
+		float kolicnik=kalkulator.deljenje(numbersStr);
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void testKodDeljanjaSaKosomCrtomNaLosemMestu() throws StringCalculatorException{
+		
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="3,\n";
+		float kolicnik=kalkulator.deljenje(numbersStr);
+		
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void deljenjeSaNulom() throws StringCalculatorException{
+		
+		StringCalculator kalkulator= new StringCalculator();
+		String numbersStr="3,0";
+		float kolicnik=kalkulator.deljenje(numbersStr);
+		
+	}
+	
 	
 }
