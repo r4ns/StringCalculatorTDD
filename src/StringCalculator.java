e35df6d2474f94c9b8631757a860f83dbf11e6ae
Ana <Ana@Ana-PC>
2017-06-07 21:16:18 +0200

    IT36/2015, IT31/2015, IT18/2015

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
-
+//Zadatak radile: Ana Gligoric IT36/2015, Aleksandra Repac IT31/2015, Jovana Savic IT18/2015
 public class StringCalculator {
 	public int add(String numbersStr) throws StringCalculatorException {
 		// Returns the sum of the numbers given in numbersStr
@@ -30,5 +30,89 @@ public class StringCalculator {
 			}
 			return zbir;
 		}
+		
+		
 	}
+	public int substract(String numbersStr) throws StringCalculatorException {
+		String delimiter1 = ",";
+		String delimiter2 = "\n";
+		numbersStr = numbersStr.replaceAll(delimiter2, delimiter1);
+		String[] split_string = numbersStr.split(delimiter1);
+		
+		
+		
+		if(numbersStr == " ")
+			return 0;
+		else{
+			int rezultat=0;
+			if(split_string.length==1)
+			{
+				throw new StringCalculatorException();
+			}
+			for(int i = 0; i<split_string.length;i++){
+				if (i == 0)
+					rezultat =Integer.parseInt( split_string[0]);
+				if(Integer.parseInt(split_string[i])<0)
+					throw new StringCalculatorException();
+				if(i>0)
+					rezultat-=Integer.parseInt(split_string[i]);
+			
+		}
+		return rezultat;
+	}
+		
+}
+	public int product(String numberStr) throws StringCalculatorException{
+		String delimiter1 = ",";
+		String delimiter2 = "\n";
+		 numberStr = numberStr.replaceAll(delimiter2, delimiter1);
+		String[] split_string = numberStr.split(delimiter1);
+		
+		
+		
+		if(numberStr == " ")
+			return 0;
+		else{
+			int rezultat=1;
+			if(split_string.length==1)
+			{
+				throw new StringCalculatorException();
+			}
+			for(int i = 0; i<split_string.length;i++){
+				if(Integer.parseInt(split_string[i])<0)
+					throw new StringCalculatorException();
+				rezultat*=Integer.parseInt(split_string[i]);
+			
+		}
+		return rezultat;
+	}
+}
+	public int division(String numbersStr) throws StringCalculatorException{
+		String delimiter1 = ",";
+		String delimiter2 = "\n";
+		 numbersStr = numbersStr.replaceAll(delimiter2, delimiter1);
+		String[] split_string = numbersStr.split(delimiter1);
+		
+		
+		
+		if(numbersStr == " ")
+			return 0;
+		else{
+			int rezultat=0;
+			if(split_string.length==1)
+			{
+				throw new StringCalculatorException();
+			}
+			for(int i = 0; i<split_string.length;i++){
+				if (i == 0)
+					rezultat =Integer.parseInt( split_string[0]);
+				if(Integer.parseInt(split_string[i])<0)
+					throw new StringCalculatorException();
+				if(i>0)
+					rezultat/=Integer.parseInt(split_string[i]);
+			
+		}
+		return rezultat;
+	}
+}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -40,4 +40,113 @@ public class StringCalculatorTest {
 		int result= calculator.add("-1,2,3");
 		
 	}
+	@Test
+	public void test_resultWhenSteringIsEmpty_substract() throws StringCalculatorException {
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.substract(" ");
+		assertEquals("Not valid result", 0, result);
+	}
+	@Test
+	public void test_ResultWithComas_substract() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.substract("1,2,3");
+		assertEquals("Not valid result", -4 , result);
+	}
+	@Test
+	public void test_ResultWithNewLine_substract() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.substract("1\n2\n3");
+		assertEquals("Not valid result", -4 , result);
+	}
+	@Test
+	public void test_ResultWithNewLineAndComa_substract() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.substract("1,2\n3");
+		assertEquals("Not valid result", -4 , result);
+	}
+	@Test (expected=StringCalculatorException.class)
+	public void test_exceptionOneNumber_substract() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.substract("1,");
+		
+	}
+	@Test (expected=StringCalculatorException.class)
+	public void test_exceptionNegativeNumber_substract() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.substract("-1,2,3");
+		
+	}
+	@Test
+	public void test_resultWhenSteringIsEmpty_product() throws StringCalculatorException {
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.product(" ");
+		assertEquals("Not valid result", 0, result);
+	}
+	@Test
+	public void test_ResultWithComas_product() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.product("1,2,3");
+		assertEquals("Not valid result", 6 , result);
+	}
+	@Test
+	public void test_ResultWithNewLine_product() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.product("1\n2\n3");
+		assertEquals("Not valid result", 6 , result);
+	}
+	@Test
+	public void test_ResultWithNewLineAndComa_product() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.product("1,2\n3");
+		assertEquals("Not valid result", 6 , result);
+	}
+	@Test (expected=StringCalculatorException.class)
+	public void test_exceptionOneNumber_product() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.product("1,");
+		
+	}
+	@Test (expected=StringCalculatorException.class)
+	public void test_exceptionNegativeNumber_product() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.product("-1,2,3");
+		
+	}
+	@Test
+	public void test_resultWhenSteringIsEmpty_division() throws StringCalculatorException {
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.division(" ");
+		assertEquals("Not valid result", 0, result);
+	}
+	@Test
+	public void test_ResultWithComas_division() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.division("10,5,2");
+		assertEquals("Not valid result", 1 , result);
+	}
+	@Test
+	public void test_ResultWithNewLine_division() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.division("10\n5\n2");
+		assertEquals("Not valid result", 1 , result);
+	}
+	@Test
+	public void test_ResultWithNewLineAndComa_division() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.division("10,5\n2");
+		assertEquals("Not valid result",1 , result);
+	}
+	@Test (expected=StringCalculatorException.class)
+	public void test_exceptionOneNumber_division() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.division("1,");
+		
+	}
+	@Test (expected=StringCalculatorException.class)
+	public void test_exceptionNegativeNumber_division() throws StringCalculatorException{
+		StringCalculator calculator = new StringCalculator();
+		int result= calculator.division("-1,2,3");
+		
+	}
+	
 }
