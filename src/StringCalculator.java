ebef5a310cef49caa9a4a8372dbda03dfa99032e
gavri <gavri@LAPTOP-T8C9NG9S>
2017-06-08 10:50:19 +0200

    StringCalculator TDD doradjeno

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -4,37 +4,111 @@ import org.junit.Test;
 
 public class StringCalculator {
 	public int add(String numbersStr) throws StringCalculatorException {
+		
+		int rezultat = 0;
+		
 		if (numbersStr == "") {
 			throw new StringCalculatorException("Dati input nije validan");	
 			
 		} else if (numbersStr.contains("-")) {
 			throw new StringCalculatorException("Nije moguce vrsiti datu operaciju sa negativnim brojevima");
 			
-		} else if (numbersStr.contains(",")) {
-			String[] niz = numbersStr.split(",");	
-			int rezultat = 0;
-			for(int i=0; i < niz.length; i++) {
-				rezultat = rezultat + Integer.parseInt(niz[i]);
-			}
-			return rezultat;
-			
-		} else if (numbersStr.contains("\n")) {
-			String[] niz = numbersStr.split("\n");	
-			int rezultat = 0;
-			for(int i=0; i < niz.length; i++) {
-				rezultat = rezultat + Integer.parseInt(niz[i]);
-			}
-			return rezultat;
-		} else if (numbersStr.contains(",\n")) {
-			String[] niz = numbersStr.split(",\n");	
-			int rezultat = 0;
-			for(int i=0; i < niz.length; i++) {
-				rezultat = rezultat + Integer.parseInt(niz[i]);
-			}
-			return rezultat;
-		} 	
+		} 
 		
-		return 0;	
+		String[] brojevi = numbersStr.split(",|\\\n");
+		
+		for (int i=0; i < brojevi.length; i++) {
+			rezultat = rezultat + Integer.parseInt(brojevi[i]);
+		}
+		
+		if (rezultat == Integer.parseInt(brojevi[0])) {
+			throw new StringCalculatorException("Dati input nije validan");
+		}
+		
+		return rezultat;
+	}
+	
+	public int substract(String numbersStr) throws StringCalculatorException {
+		 
+		int rezultat = 0;
+		
+		if (numbersStr == "") {
+			throw new StringCalculatorException("Dati input nije validan");	
+			
+		} else if (numbersStr.contains("-")) {
+			throw new StringCalculatorException("Nije moguce vrsiti datu operaciju sa negativnim brojevima");
+			
+		} 
+		
+		String[] brojevi = numbersStr.split(",|\\\n");
+		
+		for (int i=0; i < brojevi.length; i++) {
+			if (i == 0) {
+				rezultat = Integer.parseInt(brojevi[i]);				
+			} else {
+				rezultat = rezultat - Integer.parseInt(brojevi[i]);
+			}
+		}
+		
+		if (rezultat == Integer.parseInt(brojevi[0])) {
+			throw new StringCalculatorException("Dati input nije validan");
+		}
+		
+		return rezultat;
+	}
+	
+	public int multiply(String numbersStr) throws StringCalculatorException {
+		
+		int rezultat = 1;
+		
+		if (numbersStr == "") {
+			throw new StringCalculatorException("Dati input nije validan");	
+			
+		} else if (numbersStr.contains("-")) {
+			throw new StringCalculatorException("Nije moguce vrsiti datu operaciju sa negativnim brojevima");
+			
+		} 
+		
+		String[] brojevi = numbersStr.split(",|\\\n");
+		
+		for (int i=0; i < brojevi.length; i++) {
+			rezultat = rezultat * Integer.parseInt(brojevi[i]);
+		}
+		
+		if (rezultat == Integer.parseInt(brojevi[0])) {
+			throw new StringCalculatorException("Dati input nije validan");
+		}		
+		
+		return rezultat;
+	}
+	
+	public float divide(String numbersStr) throws StringCalculatorException {
+		 
+		float rezultat = 0;
+		
+		if (numbersStr == "") {
+			throw new StringCalculatorException("Dati input nije validan");	
+			
+		} else if (numbersStr.contains("-")) {
+			throw new StringCalculatorException("Nije moguce vrsiti datu operaciju sa negativnim brojevima");
+			
+		} 
+		
+		String[] brojevi = numbersStr.split(",|\\\n");
+		
+		for (int i=0; i < brojevi.length; i++) {
+			if (i == 0) {
+				rezultat = Float.parseFloat(brojevi[i]);				
+			} else {
+				rezultat = rezultat / Float.parseFloat(brojevi[i]);
+			}
+		}
+		
+		if (rezultat == Float.parseFloat(brojevi[0])) {
+			throw new StringCalculatorException("Dati input nije validan");
+		}
+		
+		return rezultat;
 	}
 	
 	public StringCalculator() {
