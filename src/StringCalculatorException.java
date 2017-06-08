68a33b8ee53f8124943ff6da95b8eabe0c614845
Aleksandar Stojic <aleksandarstojic@somethinghehe.com>
2017-06-08 11:24:27 +0200

    Final commit, task completed

--- src/StringCalculatorException.java
+++ src/StringCalculatorException.java
@@ -1,4 +1,9 @@
 
 public class StringCalculatorException extends Exception {
 
+	//Custom Exception with a custom message
+	public StringCalculatorException(String message)
+	{
+		super(message);
+	}
 }
