676450929673872b60b0e2750a03f38a41365887
Nemanja <Nemanja@Nemanja-PC>
2017-06-08 14:36:19 +0200

    Calculator

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,112 @@
-
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
+	
+	public int add(String numbersStr) throws StringCalculatorException {
 		
-		// not yet implemented
-		return 0;
+		String str[]  = numbersStr.split(",|\\\n");
+		int rez=0;
+		
+		if(numbersStr == "")
+		{
+			throw new StringCalculatorException("Prazan string!");
+		}
+		
+		for (int i=0; i<str.length; i++)
+		{
+			if(Integer.parseInt(str[i]) > 0)
+			{
+				rez += Integer.parseInt(str[i]);
+			}
+			else
+			{
+				throw new StringCalculatorException("Broj manji od nule!");
+			}
+		}
+		return rez;
+			
 	}
+	
+	public int oduzimanje(String numbersStr) throws StringCalculatorException {
+		
+		String str[]  = numbersStr.split(",|\\\n");
+		int rez= 0;
+		
+		if(numbersStr == "")
+		{
+			throw new StringCalculatorException("Prazan string!");
+		}
+		
+		for (int i=0; i<str.length; i++)
+		{
+			
+			if(Integer.parseInt(str[i]) > 0)
+			{
+				rez -= Integer.parseInt(str[i]);
+			}
+			else
+			{
+				throw new StringCalculatorException("Broj manji od nule!");
+			}
+		}
+		return rez;
+			
+	}
+	
+	
+	public int mnozenje(String numbersStr) throws StringCalculatorException {
+
+		String str[]  = numbersStr.split(",|\\\n");
+		int rez= 1;
+
+		if(numbersStr == "")
+		{
+			throw new StringCalculatorException("Prazan string!");
+		}
+
+		for (int i=0; i<str.length; i++)
+		{
+
+			if(Integer.parseInt(str[i]) > 0)
+			{
+				rez *= Integer.parseInt(str[i]);
+			}
+			else
+			{
+				throw new StringCalculatorException("Broj manji od nule!");
+			}
+		}
+		return rez;
+
+	}
+	
+	public float deljenje(String numbersStr) throws StringCalculatorException {
+
+		String str[]  = numbersStr.split(",|\\\n");
+		float rez= 1;
+
+		if(numbersStr == "")
+		{
+			throw new StringCalculatorException("Prazan string!");
+		}
+
+		for (int i=0; i<str.length; i++)
+		{
+			if(i==0)
+			{
+				rez = Float.parseFloat(str[0]);
+			}
+			if(i>0)
+			{
+				rez /= Float.parseFloat(str[i]);
+			}	
+			if(i<0)
+			{
+				throw new StringCalculatorException("Broj manji od nule!");
+			}
+			
+		}
+		return rez;
+
+	}
+	
+
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -4,9 +4,109 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
+	@Test (expected = StringCalculatorException.class)
+	public void test() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(calc.add(" "), 0);
 	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void Add_test1() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(calc.add("1,2,3"),6);
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void Add_test2() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(calc.add("1\n2,3"),6);
+	}
+	
+	@Test (expected = StringCalculatorException.class)	
+	public void Add_test3() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		calc.add("1,-2,3");
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void Oduzimanje_test1() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(calc.oduzimanje("5,2,1"),2);
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void Oduzimanje_test2() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(calc.oduzimanje("6\n4,1"),1);
+	}
+	
+	@Test (expected = StringCalculatorException.class)	
+	public void Oduzimanje_test3() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		calc.oduzimanje("1,-5,3");
+	}
+	@Test (expected = StringCalculatorException.class)
+	public void Oduzimanje_test4() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(calc.oduzimanje("6,2\n1"),3);
+	}
+	@Test (expected = StringCalculatorException.class)	
+	public void Oduzimanje_test5() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		calc.oduzimanje("1,\n");
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void Mnozenje_test1() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(calc.mnozenje("1,2,3"),6);
+	}
+	
+	
+	@Test (expected = StringCalculatorException.class)
+	public void Mnozenje_test2() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(calc.mnozenje("1\n4,3"),12);
+	}
+	
+	@Test (expected = StringCalculatorException.class)	
+	public void Mnozenje_test3() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		calc.mnozenje("1,-5,3");
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void Mnozenje_test4() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(calc.mnozenje("1,2\n3"),6);
+	}
+	
+	
+	@Test (expected = StringCalculatorException.class)
+	public void Deljenje_test1() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(calc.deljenje("25,5,1"),5);
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void Deljenje_test2() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		assertEquals(calc.deljenje("48\n6,8"),1);
+	}
+	
+	@Test (expected = StringCalculatorException.class)
+	public void Deljenje_test3() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		calc.deljenje("8,-5,3");
+	}
+	
+	
+	
+	
+	
+
+	
+	
+	
 
 }
