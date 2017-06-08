ce10e06476f693a86d5f6fb8e373d8773353aa66
Macne <Macne@DESKTOP-3PA7667>
2017-06-08 16:19:42 +0200

    Probni Test 2

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,89 @@
+import java.util.ArrayList;
 
 public class StringCalculator {
-	public int add(String numbersStr) {
-		// Returns the sum of the numbers given in numbersStr
+	private ArrayList<String> brojevi = new ArrayList<String>();
+	
+	public StringCalculator(){
 		
-		// not yet implemented
+	}
+	
+	public int add(String numbersStr) throws StringCalculatorException {
+		
+		String[] broj = numbersStr.split(",");
+		
+		for(int i = 0; i < broj.length; i++)
+		{
+			//System.out.println("Vrednosti stringa: "+broj[i]);
+			if(!isNumeric(broj[i]))
+			{
+				throw new StringCalculatorException();
+			}
+			else
+				brojevi.add(broj[i]);
+		}
 		return 0;
+	}
+	public int sabiranje()
+	{
+		int rez = 0;
+		for(int i = 0; i < brojevi.size(); i++)
+		{
+			rez += Integer.parseInt(brojevi.get(i));
+		}
+		return rez;
+	}
+	
+	public int oduzimanje()
+	{
+		int rez = 0;
+		for(int i = 0; i < brojevi.size(); i++)
+		{
+			rez -= Integer.parseInt(brojevi.get(i));
+		}
+		return rez;
+	}
+	
+	public int mnozenje()
+	{
+		int rez = 1;
+		for(int i = 0; i < brojevi.size(); i++)
+		{
+			rez *= Integer.parseInt(brojevi.get(i));
+		}
+		return rez;
+	}
+	
+	public String deljenje()
+	{
+		String izlaz = null;
+		double rez = Double.parseDouble(brojevi.get(0));;
+		for(int i = 1; i < brojevi.size(); i++)
+		{
+			if(Double.parseDouble(brojevi.get(i)) == 0)
+			{
+				izlaz = "Deljenje sa 0 nije moguce";
+				break;
+			}
+			else
+				rez /= Double.parseDouble(brojevi.get(i));
+			izlaz = Double.toString(rez);
+		}
+		
+		return izlaz;
+	}
+	
+	
+	
+	public static boolean isNumeric(String str)  
+	{  
+	  try  
+	  {  
+	    double d = Double.parseDouble(str);
+	  }  
+	  catch(NumberFormatException nfe)  
+	  {  
+	    return false;  
+	  }  
+	  return true;  
 	}
 }
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -3,10 +3,41 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringCalculatorTest {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void test_Zbir() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		calc.add("1,2,3");
+		assertEquals("Ocekivana vrednost zbira je 6",6,calc.sabiranje());
 	}
+	
+	@Test
+	public void test_Razlika() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		calc.add("1,2,3");
+		assertEquals("Ocekivana vrednost zbira je -6",-6,calc.oduzimanje());
+	}
+	
+	@Test
+	public void test_Proizvod() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		calc.add("1,2,3");
+		assertEquals("Ocekivana vrednost zbira je 6",6,calc.mnozenje());
+	}
+	
+	@Test
+	public void test_Kolicnik() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		calc.add("6,2");
+		assertEquals("Ocekivana vrednost zbira je 3.0","3.0",calc.deljenje());
+	}
+	
+	@Test
+	public void test_deljenjeSaNulom() throws StringCalculatorException{
+		StringCalculator calc = new StringCalculator();
+		calc.add("6,0");
+		assertEquals("Ocekivana vrednost zbira je 3.0","Deljenje sa 0 nije moguce",calc.deljenje());
+	}
+	
 
 }
