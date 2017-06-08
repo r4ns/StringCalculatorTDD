fd76aec698aec8d283333297e35615cd5f1938ba
damja <damja@DESKTOP-V8IAIOV>
2017-06-08 19:36:29 +0200

    IT46/2015

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,88 @@
 
 public class StringCalculator {
-	public int add(String numbersStr) {
+	public int add(String numbersStr) throws StringCalculatorException {
 		// Returns the sum of the numbers given in numbersStr
-		
-		// not yet implemented
-		return 0;
+		if(numbersStr=="")
+			return 0;
+		else{
+		String[] nizBrojeva = numbersStr.split("[\n,]");
+		int[] brojevi= new int[nizBrojeva.length];
+		int rezultat = 0;
+		for(int i=0; i<nizBrojeva.length;i++){
+			brojevi[i]=Integer.parseInt(nizBrojeva[i]);
+			if(brojevi[i]>=0){
+			rezultat+=brojevi[i];
+			}
+			else 
+				throw new StringCalculatorException("Nije moguce vrsiti datu operaciju sa negativnim brojevima");
+		}
+		return rezultat;
+		}
 	}
+	
+	public int substraction(String numbersStr) throws StringCalculatorException{
+		if(numbersStr.length()==0){
+			return 0;
+		}
+		else{
+			String[] nizBrojeva = numbersStr.split("[\n,]");
+			int[] brojevi= new int[nizBrojeva.length];
+			for(int i=0; i<brojevi.length; i++){
+				brojevi[i]=Integer.parseInt(nizBrojeva[i]);
+				if(brojevi[i]<0)
+					throw new StringCalculatorException("Nije moguce vrsiti datu operaciju sa negativnim brojevima");
+			}
+			int rezultat = brojevi[0];
+			for(int i=1; i<brojevi.length; i++){
+				rezultat-=brojevi[i];
+				
+			}
+			return rezultat;
+		}
+	}
+	
+	public int divide(String numbersStr) throws StringCalculatorException{
+		if(numbersStr.length()==0){
+			return 0;
+		}
+		else{
+			String[] nizBrojeva = numbersStr.split("[\n,]");
+			int[] brojevi= new int[nizBrojeva.length];
+			for(int i=0; i<brojevi.length; i++){
+				brojevi[i]=Integer.parseInt(nizBrojeva[i]);
+				if(brojevi[i]<0)
+					throw new StringCalculatorException("Nije moguce vrsiti datu operaciju sa negativnim brojevima");
+			}
+			int rezultat = brojevi[0];
+			for(int i=1; i<brojevi.length; i++){
+				rezultat/=brojevi[i];
+				
+			}
+			return rezultat;
+		}
+	}
+	
+	public int multiple(String numbersStr) throws StringCalculatorException{
+		if(numbersStr.length()==0){
+			return 0;
+		}
+		else{
+			String[] nizBrojeva = numbersStr.split("[\n,]");
+			int[] brojevi= new int[nizBrojeva.length];
+			for(int i=0; i<brojevi.length; i++){
+				brojevi[i]=Integer.parseInt(nizBrojeva[i]);
+				if(brojevi[i]<0)
+					throw new StringCalculatorException("Nije moguce vrsiti datu operaciju sa negativnim brojevima");
+			}
+			int rezultat = brojevi[0];
+			for(int i=1; i<brojevi.length; i++){
+				rezultat*=brojevi[i];
+				
+			}
+			return rezultat;
+		}
+	}
+	
+	
 }
+
--- tests/StringCalculatorTest.java
+++ tests/StringCalculatorTest.java
@@ -4,9 +4,141 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testConstructor(){
+		StringCalculator s1 = new StringCalculator();
+		assertNotNull(s1);
 	}
-
+	
+	@Test
+	public void testAddTwoNumbers() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj = s1.add("1,2");
+		assertEquals(3,broj);
+	}
+	
+	@Test
+	public void testAddTwoNumbs() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj=s1.add("1\n2");
+		assertEquals(3,broj);
+	}
+	
+	@Test
+	public void testNull() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj=s1.add("");
+		assertEquals(0,broj);
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void textException() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj = s1.add("-1");
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void textExceptionTwoNums() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj = s1.add("-1,2");
+	}
+	
+	@Test
+	public void SubstractionTwoNumbers() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj=s1.substraction("2,1");
+		assertEquals(1,broj);
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void NegativeSubstraction() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj=s1.substraction("-1,2");
+	}
+	
+	public void testNullSubstraction() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj=s1.substraction("");
+		assertEquals(0,broj);
+	}
+	@Test
+	public void testAddTwoNumbsSubstraction() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj=s1.substraction("2\n1");
+		assertEquals(1,broj);
+	}
+	
+	//DIVIDE
+	
+	@Test
+	public void testDivideTwoNumbers() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj = s1.divide("4,2");
+		assertEquals(2,broj);
+	}
+	
+	@Test
+	public void testDivideTwoNumbs() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj=s1.divide("4\n2");
+		assertEquals(2,broj);
+	}
+	
+	@Test
+	public void testDivideNull() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj=s1.divide("");
+		assertEquals(0,broj);
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void textDivideException() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj = s1.divide("-1");
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void textDivideTwoNums() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj = s1.divide("-1,2");
+	}
+	
+	//MULTIPLE
+	
+	@Test
+	public void testMultipleTwoNumbers() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj = s1.multiple("4,2");
+		assertEquals(8,broj);
+	}
+	
+	@Test
+	public void testMultipleTwoNumbs() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj=s1.multiple("4\n2");
+		assertEquals(8,broj);
+	}
+	
+	@Test
+	public void testMultipleNull() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj=s1.multiple("");
+		assertEquals(0,broj);
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void textMultipleException() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj = s1.multiple("-1");
+	}
+	
+	@Test(expected=StringCalculatorException.class)
+	public void textMultpleTwoNums() throws StringCalculatorException{
+		StringCalculator s1 = new StringCalculator();
+		int broj = s1.multiple("-1,2");
+	}
+	
+	
+	
 }
